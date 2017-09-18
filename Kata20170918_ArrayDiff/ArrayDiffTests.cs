5e2396ef4b89762b3558d77de6851b11e8669dc1
cash <cash@cashwu.com>
2017-09-18 22:11:21 +0800

    input array { 1,2,2 } and array { 1 } test case

--- Kata20170918_ArrayDiff/ArrayDiffTests.cs
+++ Kata20170918_ArrayDiff/ArrayDiffTests.cs
@@ -31,6 +31,12 @@ namespace Kata20170918_ArrayDiff
             ArrayDiffShouldBe(new int[] {}, new int[] { }, new[] { 1 });
         }
 
+        [TestMethod]
+        public void Input_array_1_2_2_and_array_1_should_return_array_2_2()
+        {
+            ArrayDiffShouldBe(new[] { 2, 2 }, new[] { 1, 2, 2 }, new[] { 1 });
+        }
+
         private static void ArrayDiffShouldBe(int[] expected, int[] a, int[] b)
         {
             var kata = new Kata();
@@ -43,8 +49,7 @@ namespace Kata20170918_ArrayDiff
     {
         public int[] ArrayDiff(int[] a, int[] b)
         {
-            return a.Except(b).ToArray();
-            // return a.Where(i => !b.Contains(i)).ToArray();
+            return a.Where(i => !b.Contains(i)).ToArray();
         }
     }
 }
