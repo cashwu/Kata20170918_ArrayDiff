c7fa1f565d52b89b271471014a5e1b0c08a0b514
cash <cash@cashwu.com>
2017-09-18 21:41:06 +0800

    input int { 1 } and empty test case

--- Kata20170918_ArrayDiff/ArrayDiffTests.cs
+++ Kata20170918_ArrayDiff/ArrayDiffTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170918_ArrayDiff
     public class ArrayDiffTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_array_1_and_empty_array_should_return_array_1()
         {
+            ArrayDiffShouldBe(new[] { 1 }, new[] { 1 }, new int[] { });
+        }
+
+        private static void ArrayDiffShouldBe(int[] expected, int[] a, int[] b)
+        {
+            var kata = new Kata();
+            var actual = kata.ArrayDiff(a, b);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a;
         }
     }
 }
