2d090e09ecf933b83b95df6a701b1c360e481d22
cash <cash@cashwu.com>
2017-09-18 21:35:06 +0800

    Add project files.

---
+++ Kata20170918_ArrayDiff/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170918_ArrayDiff
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
